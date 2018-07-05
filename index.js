799d90cf333a78cff791eb32dc5c35638da51952
justus <justus@localhost.localdomain>
2018-07-05 08:20:29 +0300

    index muutoksia

--- index.js
+++ index.js
@@ -1,83 +1,26 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const express = require('express');
+const app = express();
+const Botti = require(`./bot.js`);
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+Botti.run();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+app.get('/', function (req, res) {
+  	res.send("Botti rullaa")
+})
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
+// app.get('/news', function (req, res) {
+//   dbNews.find({}).sort({
+//     updatedAt: -1
+//   }).exec(function (err, uutiset) {
+//     let tuloste = ""
+//     if (err) res.send(err);
+//     uutiset.forEach(uutinen => {
+//       tuloste += "<br><br>" + uutinen.otsikko;
+//     })
+//     res.send(tuloste)
+//   });
+// });
 
-const cooldowns = new Discord.Collection();
+app.listen(process.env.PORT || 8080)
 
-client.on('ready', () => {
-	console.log('Ready!');
-});
-
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command){
-		message.delete();
-		message.reply('Komentoa ei ole olemassa!');
-		return;
-	} 
-
-	if (command.guildOnly && message.channel.type !== 'text') {
-		return message.reply('I can\'t execute that command inside DMs!');
-	}
-
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.channel.send(reply);
-	}
-
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
-
-	if (!timestamps.has(message.author.id)) {
-		timestamps.set(message.author.id, now);
-		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-	}
-	else {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		}
-
-		timestamps.set(message.author.id, now);
-		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-	}
-
-	try {
-		command.execute(message, args);
-	}
-	catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
-});
-
-client.login(token);
+console.log('Botti hengittää kolossa 8080');
